4e3cc8658fd3b2f238731af6593b16c50215364c
DaeL11 <60555590+DaeL11@users.noreply.github.com>
2021-08-19 09:19:54 -0500

    Modificamos main para hacer saludo personalizado, nuevas carpetas y nueva interface Shape

--- Patterns/src/main/java/edu/eci/cvds/patterns/App.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -1,13 +1,21 @@
 package edu.eci.cvds.patterns;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args)
     {
-        System.out.println( "Hello World!" );
+        if(args.length > 0){ 
+            System.out.println( "Hello " + Arrays.toString(args).replace("[", "").replace("]", "").replace(",", "") + "!");
+        }
+        else{
+            System.out.println( "Hello World!!");
+        }
+        
     }
 }
